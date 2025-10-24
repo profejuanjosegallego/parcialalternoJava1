10433304b16211bc36d045d439fb28cc944cae06
Emanuel_Celis <ecelis813@gmail.com>
2025-10-24 18:26:25 -0500

    Feat: se agrego las variables de color, creacion usuario y contraseña

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,11 +1,20 @@
 package org.example;
 
+import java.util.InputMismatchException;
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
 
         Scanner keyEntry = new Scanner(System.in);
-        Integer menuOption=null;
+        Integer menuOption=0;
         String greenColor="\u001B[32m";
+        String yellowColor ="\u001B[33m";
+        String redColor ="\u001B[31m";
+        String usuarioDigitado = "";
+        String usuarioAlmacenado = "user";
+        String contraseñaDigitado = "";
+        String contraseñaAlmacenado ="admin123";
 
         //implementa logica para login con:
         //-nombre usuario
@@ -26,7 +35,7 @@ public class Main {
             try {
 
                 System.out.println("👕 Bienvenido a gestor de prendas...\n¿Qué quieres realizar?\n");
-                System.out.println(GREEN + "1) Guardar una prenda en BD 📝");
+                System.out.println(greenColor + "1) Guardar una prenda en BD 📝");
                 System.out.println("2) Mostrar el inventario de prendas 📦");
                 System.out.println("3) SALIR ❌\n");
 
@@ -38,40 +47,41 @@ public class Main {
                 if (menuOption == 1) {
                     try {
                         System.out.print("Nombre de la prenda: ");
-                        String nombre = nextLine();
+                        String nombre = keyEntry.nextLine();
 
                         System.out.print("Talla (S/M/L/XL): ");
-                        String talla = sc.nextLine();
+                        String talla = keyEntry.nextLine();
 
                         System.out.print("Precio (entero): ");
-                        int precio = Integer.parseInt(sc.nextLine().trim());
-                        System.out.println(GREEN + "✅ Prenda guardada: " + nombre + " - " + talla + " - $" + precio);
+                        int precio = Integer.parseInt(keyEntry.nextLine().trim());
+                        System.out.println(greenColor + "✅ Prenda guardada: " + nombre + " - " + talla + " - $" + precio);
                     } catch (NumberFormatException e) {
-                        System.out.println(RED + "Precio inválido. Debe ser un número entero." );
+                        System.out.println(redColor + "Precio inválido. Debe ser un número entero." );
                     }
                 } else if (menuOption == 2) {
 
                     //implementar algoritmo para recorrer y mostrar la lista en el formato pedido
-                    System.out.println(YELLOW + "📋 Inventario (demo):");
+                    System.out.println(yellowColor + "📋 Inventario (demo):");
                     System.out.println("- Camiseta | M | $45000");
                     System.out.println("- Jean     | L | $120000");
 
 
                 } else if (menuOption == 3) {
                     //implementar algoritmo para cerrar el programa
+                    break;
                 } else {
-                    System.out.println(RED + "Opción no válida. Prueba con un número del 1 al 5.");
+                    System.out.println(redColor + "Opción no válida. Prueba con un número del 1 al 5.");
                 }
 
 
-                if (menuOption != null && menuOption != 5) {
+                if (menuOption != 5) {
                     System.out.print("\nPresiona ENTER para continuar...");
                     keyEntry.nextLine();
                     System.out.println();
                 }
 
             } catch (InputMismatchException ex) {
-                System.out.println(RED + "Entrada inválida. Debes digitar un número (1-5).");
+                System.out.println(redColor + "Entrada inválida. Debes digitar un número (1-5).");
                 keyEntry.nextLine();
                 menuOption = -1;
                 System.out.print("\nPresiona ENTER para continuar...");
@@ -79,13 +89,13 @@ public class Main {
                 System.out.println();
             } catch (Exception ex) {
                 // Cualquier otra excepción no prevista
-                System.out.println(RED + "Ups, algo salió mal: " + ex.getMessage());
+                System.out.println(redColor + "Ups, algo salió mal: " + ex.getMessage());
                 System.out.print("\nPresiona ENTER para continuar...");
                 keyEntry.nextLine();
                 System.out.println();
             }
 
-        } while (menuOption == null || menuOption != 5);
+        } while (menuOption != 5);
 
     }
 }

5a87edafd7c4f01526acfd344c8d4635dd7fbc8a
Sara Galeano <saragaleo67@gmail.com>
2025-10-24 18:25:10 -0500

    añadir importaciones del Scanner

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,4 +1,5 @@
 package org.example;
+import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
@@ -11,8 +12,6 @@ public class Main {
         //-nombre usuario
         //-correo
         //-contraseña
-
-
 
         System.out.println("\n******************");
         System.out.println("***** APP *****");

a6ecea3bb00d94f49cb4ca01ec2a93d0d7b31ffc
Luis-Delgado81 <ldelgado81@gmail.com>
2025-10-24 19:41:41 -0500

    Parcial backend FEAT

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,16 +1,25 @@
 package org.example;
 
+import java.util.Scanner;
+import java.util.InputMismatchException;
+
 public class Main {
     public static void main(String[] args) {
 
         Scanner keyEntry = new Scanner(System.in);
         Integer menuOption=null;
         String greenColor="\u001B[32m";
+        String redColor = "\u001B[31m";
+        String yellowColor = "\u001B[33m";
+        String blueColor = "\u001B[34m";
 
-        //implementa logica para login con:
-        //-nombre usuario
-        //-correo
-        //-contraseña
+        String usuarioBD = "user24";
+        String correoBD = "luis.delgado@gmail";
+        String contraseñaBD = "admin123*";
+
+        String usuarioIngresado;
+        String correoIngresado;
+        String contraseñaIngresada;
 
 
 
@@ -18,15 +27,24 @@ public class Main {
         System.out.println("***** APP *****");
         System.out.println("******************");
 
+        System.out.println(blueColor+"Bienvenido a softwareMASTER 🧐");
+        System.out.println("\n Digita tu nombre de usuario: ");
+        usuarioIngresado = keyEntry.nextLine();
 
-        System.out.println("Digita una opcion: ");
-        menuOption=keyEntry.nextInt();
+        System.out.println("\n Digita tu correo: ");
+        correoIngresado = keyEntry.nextLine();
 
-        do {
+        System.out.println("\n Digita tu contraseña: ");
+        contraseñaIngresada = keyEntry.nextLine();
+
+            if (usuarioIngresado.equals(usuarioBD) && contraseñaIngresada.equals(contraseñaBD) && correoIngresado.equals(correoBD)) {
+
+
+                do {
             try {
 
                 System.out.println("👕 Bienvenido a gestor de prendas...\n¿Qué quieres realizar?\n");
-                System.out.println(GREEN + "1) Guardar una prenda en BD 📝");
+                System.out.println(greenColor + "1) Guardar una prenda en BD 📝");
                 System.out.println("2) Mostrar el inventario de prendas 📦");
                 System.out.println("3) SALIR ❌\n");
 
@@ -38,29 +56,29 @@ public class Main {
                 if (menuOption == 1) {
                     try {
                         System.out.print("Nombre de la prenda: ");
-                        String nombre = nextLine();
+                        String nombre = keyEntry.nextLine();
 
                         System.out.print("Talla (S/M/L/XL): ");
-                        String talla = sc.nextLine();
+                        String talla = keyEntry.nextLine();
 
                         System.out.print("Precio (entero): ");
-                        int precio = Integer.parseInt(sc.nextLine().trim());
-                        System.out.println(GREEN + "✅ Prenda guardada: " + nombre + " - " + talla + " - $" + precio);
+                        int precio = Integer.parseInt(keyEntry.nextLine().trim());
+                        System.out.println(greenColor + "✅ Prenda guardada: " + nombre + " - " + talla + " - $" + precio);
                     } catch (NumberFormatException e) {
-                        System.out.println(RED + "Precio inválido. Debe ser un número entero." );
+                        System.out.println(redColor+ "Precio inválido. Debe ser un número entero." );
                     }
                 } else if (menuOption == 2) {
 
                     //implementar algoritmo para recorrer y mostrar la lista en el formato pedido
-                    System.out.println(YELLOW + "📋 Inventario (demo):");
+                    System.out.println(yellowColor + "📋 Inventario (demo):");
                     System.out.println("- Camiseta | M | $45000");
                     System.out.println("- Jean     | L | $120000");
 
 
                 } else if (menuOption == 3) {
-                    //implementar algoritmo para cerrar el programa
+                    break;
                 } else {
-                    System.out.println(RED + "Opción no válida. Prueba con un número del 1 al 5.");
+                    System.out.println(redColor + "Opción no válida. Prueba con un número del 1 al 5.");
                 }
 
 
@@ -71,7 +89,7 @@ public class Main {
                 }
 
             } catch (InputMismatchException ex) {
-                System.out.println(RED + "Entrada inválida. Debes digitar un número (1-5).");
+                System.out.println(redColor + "Entrada inválida. Debes digitar un número (1-5).");
                 keyEntry.nextLine();
                 menuOption = -1;
                 System.out.print("\nPresiona ENTER para continuar...");
@@ -79,7 +97,7 @@ public class Main {
                 System.out.println();
             } catch (Exception ex) {
                 // Cualquier otra excepción no prevista
-                System.out.println(RED + "Ups, algo salió mal: " + ex.getMessage());
+                System.out.println(redColor + "Ups, algo salió mal: " + ex.getMessage());
                 System.out.print("\nPresiona ENTER para continuar...");
                 keyEntry.nextLine();
                 System.out.println();
@@ -87,5 +105,8 @@ public class Main {
 
         } while (menuOption == null || menuOption != 5);
 
-    }
-}
+    }else {
+            System.out.println("⚠️ Usuario no valido");
+            return;
+        }
+}}

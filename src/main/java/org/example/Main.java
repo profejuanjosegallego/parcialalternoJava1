df36c737a3aac10fbcc1917fdcd6c27620cb785f
Alejandro Meneses <garciamenesesale@gmail.com>
2025-11-20 14:17:20 -0500

    feat se corrigieron errores iniciales, importanto las librerias Scanner, e InputMismatchException, al igual que definir las variables de colores faltantes (amarillo y rojo)

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,11 +1,16 @@
 package org.example;
 
+import java.util.InputMismatchException;
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
 
         Scanner keyEntry = new Scanner(System.in);
         Integer menuOption=null;
+        String redColor="\u001B[31m";
         String greenColor="\u001B[32m";
+        String yellowColor="\u001B[33m";
 
         //implementa logica para login con:
         //-nombre usuario
@@ -26,7 +31,7 @@ public class Main {
             try {
 
                 System.out.println("👕 Bienvenido a gestor de prendas...\n¿Qué quieres realizar?\n");
-                System.out.println(GREEN + "1) Guardar una prenda en BD 📝");
+                System.out.println(greenColor + "1) Guardar una prenda en BD 📝");
                 System.out.println("2) Mostrar el inventario de prendas 📦");
                 System.out.println("3) SALIR ❌\n");
 
@@ -38,21 +43,21 @@ public class Main {
                 if (menuOption == 1) {
                     try {
                         System.out.print("Nombre de la prenda: ");
-                        String nombre = nextLine();
+                        String nombre = keyEntry.nextLine();
 
                         System.out.print("Talla (S/M/L/XL): ");
-                        String talla = sc.nextLine();
+                        String talla = keyEntry.nextLine();
 
                         System.out.print("Precio (entero): ");
-                        int precio = Integer.parseInt(sc.nextLine().trim());
-                        System.out.println(GREEN + "✅ Prenda guardada: " + nombre + " - " + talla + " - $" + precio);
+                        int precio = Integer.parseInt(keyEntry.nextLine().trim());
+                        System.out.println(greenColor + "✅ Prenda guardada: " + nombre + " - " + talla + " - $" + precio);
                     } catch (NumberFormatException e) {
-                        System.out.println(RED + "Precio inválido. Debe ser un número entero." );
+                        System.out.println(redColor + "Precio inválido. Debe ser un número entero." );
                     }
                 } else if (menuOption == 2) {
 
                     //implementar algoritmo para recorrer y mostrar la lista en el formato pedido
-                    System.out.println(YELLOW + "📋 Inventario (demo):");
+                    System.out.println(yellowColor + "📋 Inventario (demo):");
                     System.out.println("- Camiseta | M | $45000");
                     System.out.println("- Jean     | L | $120000");
 
@@ -60,7 +65,7 @@ public class Main {
                 } else if (menuOption == 3) {
                     //implementar algoritmo para cerrar el programa
                 } else {
-                    System.out.println(RED + "Opción no válida. Prueba con un número del 1 al 5.");
+                    System.out.println(redColor + "Opción no válida. Prueba con un número del 1 al 5.");
                 }
 
 
@@ -71,7 +76,7 @@ public class Main {
                 }
 
             } catch (InputMismatchException ex) {
-                System.out.println(RED + "Entrada inválida. Debes digitar un número (1-5).");
+                System.out.println(redColor + "Entrada inválida. Debes digitar un número (1-5).");
                 keyEntry.nextLine();
                 menuOption = -1;
                 System.out.print("\nPresiona ENTER para continuar...");
@@ -79,7 +84,7 @@ public class Main {
                 System.out.println();
             } catch (Exception ex) {
                 // Cualquier otra excepción no prevista
-                System.out.println(RED + "Ups, algo salió mal: " + ex.getMessage());
+                System.out.println(redColor + "Ups, algo salió mal: " + ex.getMessage());
                 System.out.print("\nPresiona ENTER para continuar...");
                 keyEntry.nextLine();
                 System.out.println();
